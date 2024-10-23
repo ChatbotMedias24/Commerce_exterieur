c0b2bc1183221f4b3c5b8725731b7e9373f2daf5
Chatbot <134295092+ChatbotMedias24@users.noreply.github.com>
2024-10-23 19:05:20 +0100

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,15 +1,12 @@
 import streamlit as st
+import openai
+import streamlit as st
 from dotenv import load_dotenv
 import pickle
-from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
 from langchain.llms import OpenAI
-from langchain.chains.question_answering import load_qa_chain
 from langchain.callbacks import get_openai_callback
 import os
-from langchain.chat_models import ChatOpenAI
 from langchain.chains import ConversationChain
 from langchain.chains.conversation.memory import ConversationBufferWindowMemory
 from langchain.prompts import (
@@ -22,9 +19,17 @@ from streamlit_chat import message  # Importez la fonction message
 import toml
 import docx2txt
 from langchain.memory.chat_message_histories import StreamlitChatMessageHistory
-from langchain.callbacks.base import BaseCallbackHandler
+import docx2txt
+from dotenv import load_dotenv
 if 'previous_question' not in st.session_state:
     st.session_state.previous_question = []
+
+# Chargement de l'API Key depuis les variables d'environnement
+load_dotenv(st.secrets["OPENAI_API_KEY"])
+
+# Configuration de l'historique de la conversation
+if 'previous_questions' not in st.session_state:
+    st.session_state.previous_questions = []
 
 st.markdown(
     """
@@ -39,7 +44,6 @@ st.markdown(
             margin-left: 10px;
             margin-right: -40px;
             color:black;
-
         }
 
         .assistant-message {
@@ -77,104 +81,131 @@ st.markdown(
         .message-container.assistant {
             justify-content: flex-start; /* Aligner à droite pour l'assistant */
         }
-     
+        input[type="text"] {
+            background-color: #E0E0E0;
+        }
+
+        /* Style for placeholder text with bold font */
+        input::placeholder {
+            color: #555555; /* Gris foncé */
+            font-weight: bold; /* Mettre en gras */
+        }
+
+        /* Ajouter de l'espace en blanc sous le champ de saisie */
+        .input-space {
+            height: 20px;
+            background-color: white;
+        }
+    
     </style>
     """,
     unsafe_allow_html=True
 )
-
 # Sidebar contents
 textcontainer = st.container()
 with textcontainer:
     logo_path = "medi.png"
-    logoo_path = "cp.png"
+    logoo_path = "NOTEPRESENTATION.png"
     st.sidebar.image(logo_path,width=150)
-    st.sidebar.image(logoo_path,width=150)
+   
     
 st.sidebar.subheader("Suggestions:")
 questions = [
         "Donnez-moi un résumé du rapport ",
         "Quels sont les secteurs économiques qui ont connu la plus forte croissance des exportations en 2022 ?",
         "Quels sont les principaux défis auxquels est confronté le commerce extérieur du Maroc en 2022 ?",
-        "Comment les échanges commerciaux du Maroc avec les pays membres de l'Union européenne ont-ils évolué depuis la sortie du Royaume-Uni de l'UE en 2021 ?"
-      
-    ]    
- 
+        "Comment les échanges commerciaux du Maroc avec les pays membres de l'Union européenne ont-ils évolué depuis la sortie du Royaume-Uni de l'UE en 2021 ?"  ]    
 load_dotenv(st.secrets["OPENAI_API_KEY"])
-conversation_history = StreamlitChatMessageHistory()
-
+# Initialisation de l'historique de la conversation dans `st.session_state`
+if 'conversation_history' not in st.session_state:
+    st.session_state.conversation_history = StreamlitChatMessageHistory()
 def main():
     conversation_history = StreamlitChatMessageHistory()  # Créez l'instance pour l'historique
+
     st.header("Rapport commerce extérieur du maroc 2022 💬")
-    # upload a PDF file
+    
+    # Load the document
     docx = 'CE.docx'
- 
-    # st.write(pdf)
+    
     if docx is not None:
+        # Lire le texte du document
         text = docx2txt.process(docx)
-         # Get the first page as an image
-        text_splitter = RecursiveCharacterTextSplitter(
-            chunk_size=1000,
-            chunk_overlap=200,
-            length_function=len
-            )
-        chunks = text_splitter.split_text(text=text)
- 
-        # # embeddings
-        # st.write(chunks)
- 
-        embeddings = OpenAIEmbeddings()
-        VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
-        with open("aaa.pkl", "wb") as f:
-            pickle.dump(VectorStore, f)
- 
-        # embeddings = OpenAIEmbeddings()
-        # VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
-        st.markdown('<p style="margin-bottom: 0;"><h7><b>Posez vos questions ci-dessous:</b></h7></p>', unsafe_allow_html=True)
-        query_input = st.text_input("")
-        selected_questions = st.sidebar.radio("****Choisir :****",questions)
+
+        # Afficher toujours la barre de saisie
+        st.markdown('<div class="input-space"></div>', unsafe_allow_html=True)
+        selected_questions = st.sidebar.radio("****Choisir :****", questions)
+        # Afficher toujours la barre de saisie
+        query_input = st.text_input("", key="text_input_query", placeholder="Posez votre question ici...", help="Posez votre question ici...")
+        st.markdown('<div class="input-space"></div>', unsafe_allow_html=True)
+
         if query_input and query_input not in st.session_state.previous_question:
             query = query_input
             st.session_state.previous_question.append(query_input)
         elif selected_questions:
             query = selected_questions
         else:
-            query=""
-        if query:
-            docs = VectorStore.similarity_search(query=query, k=3)
- 
-            llm = OpenAI(model="gpt-3.5-turbo-instruct")
-            chain = load_qa_chain(llm=llm, chain_type="stuff")
-            with get_openai_callback() as cb:
-                response = chain.run(input_documents=docs, question=query)
-                if "Donnez-moi un résumé du rapport" in query:
-                    response = "Le rapport annuel 2022 sur le Commerce Extérieur du Maroc présente des statistiques détaillées sur les importations et exportations de marchandises du pays. Il analyse les données par groupes de produits, principaux produits, secteurs, et partenaires commerciaux. Le rapport inclut des informations sur les transactions sous les régimes d'admission temporaire pour perfectionnement actif, ainsi que les échanges commerciaux dans le cadre des accords de libre-échange signés par le Maroc. Des aspects tels que les évolutions du commerce mondial, les principaux ratios du commerce extérieur, et l'impact des tensions géopolitiques et des fluctuations des prix des matières premières sont abordés."
+            query = ""
 
-                conversation_history.add_user_message(query)
-                conversation_history.add_ai_message(response)  # Utilisez add_ai_message
-        
-            formatted_messages = []
+        if query :
+            st.session_state.conversation_history.add_user_message(query)  # Ajouter à l'historique
 
-            for msg in conversation_history.messages:
-                role = "user" if msg.type == "human" else "assistant"
-                avatar = "🧑" if role == "user" else "🤖"
-                css_class = "user-message" if role == "user" else "assistant-message"
         
-                message_div = f'<div class="{css_class}">{msg.content}</div>'
-                avatar_div = f'<div class="avatar">{avatar}</div>'
-        
-                if role == "user":
-                    formatted_message = f'<div class="message-container user"><div class="message-avatar">{avatar_div}</div><div class="message-content">{message_div}</div></div>'
-                else:
-                    formatted_message = f'<div class="message-container assistant"><div class="message-content">{message_div}</div><div class="message-avatar">{avatar_div}</div></div>'
-        
-                formatted_messages.append(formatted_message)
+            messages = [
+                {
+                    "role": "user",
+                    "content": (
+                        f"{query}. En tenant compte du texte suivant essayer de ne pas dire le texte ne contient pas les informations si ne trouve pas à partir de texte répondre d'aprés votre connaissance stp et ne dire pas stp le texte incomplet ou incompréhensible essayer de formulé une bon réponse sans critiquer le texte par exemple ne pas dire texte fragmenter ou quelque chose comme ça répondre directement stp parceque je vais l'afficher o lecteur: {text} "
+                    )
+                }
+            ]
 
-            messages_html = "\n".join(formatted_messages)
-            st.markdown(messages_html, unsafe_allow_html=True)
+            # Appeler l'API OpenAI pour obtenir le résumé
+            response = openai.ChatCompletion.create(
+                model="gpt-4o-mini",
+                messages=messages
+            )
 
-            # Affichage des avatars à l'extérieur de la div des messages
+            # Récupérer le contenu de la réponse
+
+            summary = response['choices'][0]['message']['content']
+            if "Donnez-moi un résumé du rapport" in query:
+                summary = "Le rapport annuel 2022 sur le Commerce Extérieur du Maroc présente des statistiques détaillées sur les importations et exportations de marchandises du pays. Il analyse les données par groupes de produits, principaux produits, secteurs, et partenaires commerciaux. Le rapport inclut des informations sur les transactions sous les régimes d'admission temporaire pour perfectionnement actif, ainsi que les échanges commerciaux dans le cadre des accords de libre-échange signés par le Maroc. Des aspects tels que les évolutions du commerce mondial, les principaux ratios du commerce extérieur, et l'impact des tensions géopolitiques et des fluctuations des prix des matières premières sont abordés."
+
+                # Votre logique pour traiter les réponses
+            #conversation_history.add_user_message(query)
+            #conversation_history.add_ai_message(response)
+            st.session_state.conversation_history.add_ai_message(summary)  # Ajouter à l'historique
             
+            # Afficher la question et le résumé de l'assistant
+            #conversation_history.add_user_message(query)
+            #conversation_history.add_ai_message(summary)
+
+            # Format et afficher les messages comme précédemment
+            formatted_messages = []
+            previous_role = None 
+            if st.session_state.conversation_history.messages: # Variable pour stocker le rôle du message précédent
+                    for msg in conversation_history.messages:
+                        role = "user" if msg.type == "human" else "assistant"
+                        avatar = "🧑" if role == "user" else "🤖"
+                        css_class = "user-message" if role == "user" else "assistant-message"
+
+                        if role == "user" and previous_role == "assistant":
+                            message_div = f'<div class="{css_class}" style="margin-top: 25px;">{msg.content}</div>'
+                        else:
+                            message_div = f'<div class="{css_class}">{msg.content}</div>'
+
+                        avatar_div = f'<div class="avatar">{avatar}</div>'
+                
+                        if role == "user":
+                            formatted_message = f'<div class="message-container user"><div class="message-avatar">{avatar_div}</div><div class="message-content">{message_div}</div></div>'
+                        else:
+                            formatted_message = f'<div class="message-container assistant"><div class="message-content">{message_div}</div><div class="message-avatar">{avatar_div}</div></div>'
+                
+                        formatted_messages.append(formatted_message)
+                        previous_role = role  # Mettre à jour le rôle du message précédent
+
+                    messages_html = "\n".join(formatted_messages)
+                    st.markdown(messages_html, unsafe_allow_html=True)
 
 if __name__ == '__main__':
     main()
